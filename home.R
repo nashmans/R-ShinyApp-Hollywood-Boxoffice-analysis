16b5734a7b41ef3d5a22d80935cf15be30fb88db
nashmans <49317073+nashmans@users.noreply.github.com>
2019-04-23 02:00:58 -0400

    Update home.R

--- home.R
+++ home.R
@@ -1,16 +1,22 @@
 output$pageStub <- renderUI(tagList(
   fluidRow(
-    column(5,
-           HTML("<p>This is the home page of my workspace on Economcis of Hollywood.</p>",
+    column(10,
+           HTML(
+             "<h4> Welcome to the home page of <b> Hollywood Economics </b> .</h4>"
+           ),
+           
+           
+           HTML(
+                "<p>*<i>Click the 'Box Office' page for Box Office Revenue insights</p>",
+                "<p>  Here you can interact with two plots to study :</p>",
                 
-                "<p>The second page provide you with insights Box Office Revenue.",
-                " Here you can interact with two plots to study :",
-                "a) Revenue Trend by Major Studios vs. Minor over the past 50 yrs",
-                "b) Biggest players and market share in U.S. Domestic & Interna'l Markets",
+                "<p>   _a) Box office Trend by Major Studios vs. Minor over the past 50 yrs</p>",
+                "<p>   _b) Biggest players and their market share in U.S. Domestic & Interna'l Markets</p>",
                 
                 
-                "<p>The third link goes to a page that doesn't exist to demonstrate",
-                "error handling for bad URLs.</p>")
+                "<p>*Click on the 'Trend' page for the latest trend in spending power among hollywood Majors</p>"
+                
+             
+                )
+           
     )
-  )
-))
